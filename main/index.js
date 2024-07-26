6767ef40fe7872401b597f493cb0649c183f250c
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-26 16:19:58 +0900

    초기 실행 스크립트 간소화

--- main/index.js
+++ main/index.js
@@ -1,28 +1,10 @@
 import createRouter from "./router.js";
 import createPages from "./pages.js";
-import getContents from "./getContents.js";
 import renderNav from "./view/renderNav.js";
-import viewContent from "./view/content.js";
 
-import registry from "./registry.js";
-
-const contents = getContents();
-
-const state = {
-  contents: contents,
-};
-
-renderNav();
 const main = document.querySelector(".main");
 
-// requestAnimationFrame
-window.requestAnimationFrame(() => {
-  const newMain = registry.renderRoot(main, state);
-  main.replaceWith(newMain);
-});
-
 const pages = createPages(main);
-
 const router = createRouter();
 
 router
@@ -31,12 +13,4 @@ router
   .setNotFound(pages.notFound)
   .start();
 
-const NAV_BTN_SELECTOR = "button[data-navigate]";
-
-document.body.addEventListener("click", (e) => {
-  const { target } = e;
-  if (target.matches(NAV_BTN_SELECTOR)) {
-    const { navigate } = target.dataset;
-    router.navigate(navigate);
-  }
-});
+renderNav();
