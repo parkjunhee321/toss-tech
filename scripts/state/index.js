e987af3322c993765c38fb532f77902ad010c9d9
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-27 23:14:18 +0900

    feat: 주석 추가

--- scripts/state/index.js
+++ scripts/state/index.js
@@ -10,6 +10,7 @@ export default function useState(initialState) {
       clearTimeout(renderTimeout);
     }
 
+    // 호출되어도 마지막에 실행하기 위해 비동기 함수 추가
     renderTimeout = setTimeout(() => {
       render(state);
     }, 0);
