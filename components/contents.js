f0651448fcd0a3932f44dec4072a70763690b78a
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-29 14:28:39 +0900

    feat: divide code line

--- components/contents.js
+++ components/contents.js
@@ -1,17 +1,21 @@
 export default function Contents() {
-  return `<div class="banner-image-container">
-  <img class="banner-image" src="./assets/tosstect_banner.webp" alt="배너이미지">
-</div>
-<section class="contents-list-wrapper">
-<div class="tabs-container">
-<ul class="tabs" data-component="tabs">
-  <li class="tab">전체</li>
-  <li class="tab">개발</li>
-  <li class="tab">디자인</li>
-</ul>
-</div>
-  <div class="contents-list" data-component="contents">
-  
+  return `
+<!-->
+  <div class="banner-image-container">
+    <img class="banner-image" src="./assets/tosstech_banner.webp" alt="배너이미지">
   </div>
-</section>`;
+  <section class="contents-list-wrapper">
+    <div class="tabs-container">
+      <ul class="tabs" data-component="tabs">
+        <li class="tab">전체</li>
+        <li class="tab">개발</li>
+        <li class="tab">디자인</li>
+      </ul>
+    </div>
+    <div class="contents-list" data-component="contents">
+  
+    </div>
+  </section>
+<!-->
+`;
 }
