643e1eea9d7cdadf90a0001c73dc0cf647031e07
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-29 14:34:21 +0900

    feat: divide code line

--- view/renderNav.js
+++ view/renderNav.js
@@ -1,19 +1,20 @@
-const NavbarComponent = `<div class="nav-container">
-<div class="nav-logo">
-  <a href="/">
-    <img width="auto" height="20px" src="./assets/icon.svg" alt="로고" />
-  </a>
-</div>
-<div class="nav-contents">
-  <ul class="nav-menu">
-    <li class="nav-item">
-      <button class="nav-button blue">이메일로 소식 받기</button>
-    </li>
-    <li class="nav-item">
-      <button class="nav-button grey">채용 바로가기</button>
-    </li>
-  </ul>
-</div>
+const NavbarComponent = `
+<div class="nav-container">
+  <div class="nav-logo">
+    <a href="/">
+      <img width="auto" height="20px" src="./assets/icon.svg" alt="로고" />
+    </a>
+  </div>
+  <div class="nav-contents">
+    <ul class="nav-menu">
+      <li class="nav-item">
+        <button class="nav-button blue">이메일로 소식 받기</button>
+      </li>
+      <li class="nav-item">
+        <button class="nav-button grey">채용 바로가기</button>
+      </li>
+    </ul>
+  </div>
 </div>`;
 
 const navElement = document.querySelector("nav");
