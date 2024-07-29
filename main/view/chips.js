4a52996b0d019aa08fa59c8399c4f680efa3b8b0
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-29 12:49:35 +0900

    delete

--- main/view/chips.js
+++
@@ -1,9 +0,0 @@
-const getChipElement = (tag) => {
-  const { content } = tag;
-  return `<div class="chip">#${content}</div>`;
-};
-
-export default ({ tags }) => {
-  const chipElement = tags.map(getChipElement).join("");
-  return chipElement;
-};
