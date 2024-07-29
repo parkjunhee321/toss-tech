25d5838411edbdb7ddd76715554cca5c0227be55
parkjunhee321 <jhee.park@moyoplan.com>
2024-07-29 12:47:11 +0900

    feat: add article main text node

--- components/article.js
+++ components/article.js
@@ -1,11 +1,24 @@
-export default function Article(imageUrl, imageAlt, title, fullDescription) {
+import articleNodeInfo from "../scripts/rendering/createArticleNode.js";
+
+export default function Article(article) {
+  const { imageUrl, imageAlt, title, data } = article;
+  const result = data
+    .map((value) => {
+      return `<p>${articleNodeInfo(value)}</p>`;
+    })
+    .join("");
+
   return `<section class="article-container">
-      <img class="article-banner-image" src="${imageUrl}" alt="${imageAlt}" />
+    <div class="article-intro">
+      <img class="article-banner-image" src="${imageUrl}" alt="${
+    imageAlt || "썸네일"
+  }" />
       <h1 class="article-title">${title}</h1>
       <div class="chip-container">
       </div>
-      <div>
-      ${fullDescription}
       </div>
+      <article class="article">
+      ${result}
+      </article>
     </section>`;
 }
